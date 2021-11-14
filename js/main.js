7d0a07aaecc98ddaa711316c2aaf330b12d6bf0f
RubenJ99 <juarezperezruben13@gmail.com>
2021-11-14 13:09:48 +0100

    + creacion estructura

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(function(){
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 100) {
+			$("#toparrow").fadeIn();
+			//$("#toparrow").fadeIn("slow");
+		} else {
+			//$("#toparrow").hide();
+			$("#toparrow").fadeOut();
+		}
+	});
+
+});
