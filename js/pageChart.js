028738691509630dfe2b056ef41c53794cf9d96c
RubenJ99 <juarezperezruben13@gmail.com>
2021-11-16 01:21:08 +0100

    + bar graph todo eliminar bar

--- js/pageChart.js
+++ js/pageChart.js
@@ -9,12 +9,15 @@ const newAmount = document.getElementById('itemAmount');
 const newMonth = document.getElementById('monthId');
 const bAddSaleModal = document.getElementById('bAddSaleModal');
 
-// const monthlyLabelsSet = new Set();
-// const monthlySalesArray = [];
 
 const monthlySalesMap = new Map();
 const newProduct = document.forms[0].inlineRadioOptions; //!!
-
+const BG_COLORS = {
+	'Camera': 'rgba(238, 184, 104, 1)',
+	'Mobile': 'rgba(239, 118, 122, 1)',
+	'Laptop': 'rgba(75, 166, 223, 1)',
+	'Tablet': 'rgba(40, 167, 69, 1)',
+}
 
 const monthlySoldCameras = [];
 const monthlySoldLaptops = [];
@@ -34,7 +37,29 @@ let monthlySalesChart = new Chart(monthCtx, {
 	type: "bar",
 	data: {
 		labels: [],
-		datasets: [],
+		datasets: [{
+			label: 'Camera',
+			data: [],
+			backgroundColor: 'rgba(238, 184, 104, 1)',
+			borderWidth: 0,
+		},
+		{label: 'Laptop',
+			data: [],
+			backgroundColor: 'rgba(239, 118, 122, 1)',
+			borderWidth: 0,
+		},
+		{
+		label: 'Mobile',
+		data: [],
+		backgroundColor: 'rgba(75, 166, 223, 1)',
+		borderWidth: 0,
+		},
+		{
+		label: 'Tablet',
+		data: [],
+		backgroundColor: 'rgba(40, 167, 69, 1)',
+		borderWidth: 0,
+		},],
 	},
 	options: {
 		scales: {
@@ -69,10 +94,6 @@ let deptSalesChart = new Chart(deptCtx, {
 	options: {},
 });
 
-function addYearlyTotal(a, b, c) {
-	return a + b + c;
-}
-
 function initMonthlyTotalSales(){
 	let items = Array.from(monthlySalesMap.values());
 	let aux;
@@ -88,24 +109,6 @@ function initMonthlyTotalSales(){
 
 initMonthlyTotalSales();
 
-function findOver5000() {
-	let position = -1;
-	let quantity = monthSales.find((elem, index) => {
-		if (elem > 5000) {
-			position = index;
-			return true;
-		}
-		return false;
-	});
-	alert("Cantidad: " + quantity + " Indice: " + position);
-}
-
-
-function getSalesMonths(){
-	monthlySalesMap.forEach(function (amount, month){
-	alert(month + ": " + amount);
-	});
- }
 
 //Resetear datos en los gráficos
 function resetMonthlySales(){
@@ -141,85 +144,6 @@ function parseProd(radio) {
 	return PRODS[radio];
 }
 
-let data = {
-	label: '',
-	data: [],
-	backgroundColor: '',
-	borderWidth: 0,
-}
-const dataCam = {...data};
-const dataMob = {...data};
-const dataLap = {...data};
-const dataTab = {...data};
-
-/**
- *
- * @param {Map} monthlySalesMap
- */
-function cameraSales(monthlySalesMap) {
-	let cameras = []
-	for (const [key, val] of monthlySalesMap.entries()) {
-		if(val.has('Camera')){
-			cameras.push(val.get('Camera'))
-		}
-	}
-
-	if(!dataCam.label.localeCompare('Camera')) dataCam.label = 'Camera';
-	dataCam.data = cameras;
-	dataCam.backgroundColor = 'rgba(238,184,104,1)';
-	monthlySalesChart.data.datasets.push(dataCam);
-}
-/**
- *
- * @param {Map} monthlySalesMap
- */
- function mobileSales(monthlySalesMap) {
-	let mobiles = []
-	for (const [key, val] of monthlySalesMap.entries()) {
-		if(val.has('Mobile')){
-			mobiles.push(val.get('Mobile'))
-		}
-	}
-
-	if(!dataMob.label.localeCompare('Mobile')) dataMob.label = 'Mobile';
-	dataMob.data = mobiles;
-	dataMob.backgroundColor = 'rgba(239,118,122,1)';
-	monthlySalesChart.data.datasets.push(dataMob);
-}
-/**
- *
- * @param {Map} monthlySalesMap
- */
- function laptopSales(monthlySalesMap) {
-	let laptops = []
-	for (const [key, val] of monthlySalesMap.entries()) {
-		if(val.has('Laptop')){
-			laptops.push(val.get('Laptop'))
-		}
-	}
-
-	if(!(dataLap.label.localeCompare('Laptop'))) dataLap.label = 'Laptop';
-	dataLap.data = laptops;
-	dataLap.backgroundColor = 'rgba(75,166,223,1)';
-	monthlySalesChart.data.datasets.push(dataLap);
-}
-/**
- *
- * @param {Map} monthlySalesMap
- */
- function tabletSales(monthlySalesMap) {
-	let tablets = []
-	for (const [key, val] of monthlySalesMap.entries()) {
-		if(val.has('Tablet')){
-			tablets.push(val.get('Tablet'))
-		}
-	}
-
-	if(!dataTab.label.localeCompare('Tablet')) dataTab.label = 'Tablet';
-	dataTab.data = tablets;
-	dataTab.backgroundColor = 'rgba(40,167,69,1)';
-	monthlySalesChart.data.datasets.push(dataTab);
-}
 const PROD_POS = {
 	Camera: 0,
 	Mobile: 1,
@@ -231,67 +155,71 @@ function addSale(){
 	let product = parseProd(newProduct.value);
 	let parsedMonth = parseMonth(newMonth.value);
 	let itemsInMonth = new Map();
+	let data = {
+		label: '',
+		data: [],
+		backgroundColor: '',
+		borderWidth: 0,
+	}
+	let months = Array.from(monthlySalesMap.keys()).map(elem => parseMonth(elem));
+	console.log(months)
 		//SI EL MAPA PADRE CONTIENE EL MES SELECCIONADO
 		if (monthlySalesMap.has(newMonth.value)){
 			itemsInMonth = monthlySalesMap.get(newMonth.value); //month contiene map del mes seleccionado
 			if(itemsInMonth.has(product)){ //en caso de que tengamos un registro del item en ese mes
 				let num = itemsInMonth.get(product); //sacamos el valor del item y lo actualizamos
+				let oldNum = num;
 				num += Number.parseInt(newAmount.value);
 				itemsInMonth.set(product, num);
+				monthlySalesChart.data.datasets.forEach(dataSet => {
+					if(dataSet.label === product){
+						let indx = dataSet.data.indexOf(oldNum);
+						dataSet.data.splice(indx,1,num);
+					}
+				});
 			}else{
 				itemsInMonth.set(product, Number.parseInt(newAmount.value)); //si no añadimos el nuevo valor
+				console.log('por aqui');
+				monthlySalesChart.data.datasets.forEach(dataSet => {
+					if(dataSet.label === product){
+						console.log(product);
+						let i = months.indexOf(parseMonth(newMonth.value));
+						console.log(i);
+						dataSet.data.splice(i,1,Number.parseInt(newAmount.value));
+					}
+				});
 			}
 			monthlySalesMap.set(newMonth.value, itemsInMonth); //Acabado cualquiera de los 2 casos el mapa padre
 																									//se actualiza retornando un mapa con valores correctos para el mes elegido
 		}else{ //EN CASO DE QUE EL MAPA PADRE NO CONTENGA REGISTRO DEL MES SELECCIONADO
 			itemsInMonth.set(product, Number.parseInt(newAmount.value)); //Generamos un mapa con el item y su valor
 			monthlySalesMap.set(newMonth.value, itemsInMonth);//En el mapa padre creamos una entrada key mes val mapa de items
+			monthlySalesChart.data.datasets.forEach(dataSet => {
+				if(dataSet.label === product){
+					dataSet.data.push(Number.parseInt(newAmount.value));
+				}else{
+					dataSet.data.push(0);
+				}
+			});
 		}
-		 //Recuento de totales
-		 initMonthlyTotalSales();
-
-
-		 cameraSales(monthlySalesMap);
-		 mobileSales(monthlySalesMap);
-		 laptopSales(monthlySalesMap);
-		 tabletSales(monthlySalesMap);
-
-		if(monthLabels.indexOf(parsedMonth!==-1)){
-			monthLabels.push(parsedMonth);
-		}
-		monthlySalesChart.data.labels = monthLabels;
-
-
-
-
-
-
+		months = Array.from(monthlySalesMap.keys()).map(elem => parseMonth(elem));//Muestra los meses de los que hay registro en el grafico barras
+		monthlySalesChart.data.labels = months;
 
 		let arrPos = PROD_POS[product];
 		let n = Number.parseInt(newAmount.value);
 		let acc = dataForPie[arrPos];
 		acc += n;
 		dataForPie.splice(arrPos,1,acc);
+		deptSalesChart.data.datasets[0].data = dataForPie;
 
-
-	deptSalesChart.data.datasets[0].data = dataForPie;
-
-
-
+		console.log(monthlySalesChart.data.datasets[0].data);
+		console.log(monthlySalesChart.data.datasets[2].data);
+		//Recuento de totales
+		initMonthlyTotalSales();
 		monthlySalesChart.update();
 		deptSalesChart.update();
-
-
-
-			//  } catch (error){
-			//  // Tratamiento de excepciones
-			//  alert(error.message);
-			//  } finally {
-			//  // Reseteo de formulario
-			//  cleanAddSaleForm();
-			//  }
-			cleanAddSaleForm();
-		 }
+		cleanAddSaleForm();
+}
 
 function cleanAddSaleForm(){
 	newMonth.value = "";
